1d2a0e98d1979f98b71806d09403b44a62f8ef01
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-14 09:50:27 -0600

    add write class

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,18 @@
+# require './lib/book'
+
 class Author
   attr_reader :name,
               :books
   def initialize(name)
     @name = "#{name[:first_name]} #{name[:last_name]}"
+    @first_name = name[:first_name]
+    @last_name = name[:last_name]
     @books = []
+  end
+
+  def write(book_title, book_publication_date)
+    book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: book_title, publication_date: book_publication_date})
+    @books << book
+    book
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Author do
     it 'can write a book' do
       jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
 
-      expect(jane_eyre).to be_an_instance_of(Book)
+      expect(jane_eyre.class).to eq(Book)
       expect(jane_eyre.title).to eq('Jane Eyre')
     end
 
